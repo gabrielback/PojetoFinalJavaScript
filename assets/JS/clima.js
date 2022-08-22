be8f607d9fbe062e2069826ce20fd12bb66b2a81
gabrielback12 <gabrielluiz-back@hotmail.com>
2022-08-21 23:47:12 -0300

    Update projeto final js

--- assets/JS/clima.js
+++ assets/JS/clima.js
@@ -3,10 +3,12 @@
         const longitude = -48.85;
         const divHoje = document.getElementById("hoje");
 
+        
+        const weekday = ["Dom","Seg","Ter","Qua","Qui","Sex","Sab"];
+
         fetch("http://api.weatherapi.com/v1/forecast.json?key=870ea7f706f34eaf986213002221908&q=-26.30,-48.85&days=5&aqi=no&alerts=no")
         .then(response => response.json()) 
         .then(data => {
-            
             
             // console.log("data..current temperatura atual > ",data.current.temp_c);
             // console.log("data.location.name> ",data.location.name);
@@ -16,14 +18,15 @@
             
             
             const forecastObj = data.forecast.forecastday
+            let html = `<div class=""><p>Previsão do tempo em ${data.location.name} </p></div> `;
 
-            let html = "";
             forecastObj.forEach(element => {
-                html +=`<div class="card" style="width: 18rem;">
+                
+                html +=`<div class="card" style="width: 10rem;">
                 <img src="${element.day.condition.icon}"
                 class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">Day: ${element.date}</p>
+                <div class="card-body text-center">
+                    <p class="card-text .bg">${element.date}</p>
                     <p class="card-text">Max: ${element.day.maxtemp_c}<span>&#8451;</span> Min: ${element.day.mintemp_c}<span>&#8451;</span></p>
 
                     <p class="card-text">Comment:${element.day.condition.text}</p>
@@ -38,10 +41,6 @@
             divHoje.innerHTML = html
             
         })
-        
-        const sleep = async timeInMs => setTimeout(() => {
-            console.log(`I'm here ${timeInMs/1000} Secs later...`)
-        }, timeInMs);
 
         async function funcao01(dados){
             let dadoProcessado = ""
