0f0a961a9e87cdda6622228103ec402644f0bbaf
gabrielback12 <gabrielluiz-back@hotmail.com>
2022-08-22 13:42:40 -0300

    Update APIs && diretórios

--- assets/JS/clima.js
+++ assets/JS/clima.js
@@ -1,64 +1,47 @@
+// get location
+const currentLocation = (position) => position.coords.latitude + "," + position.coords.longitude;
+const latLong = () => navigator.geolocation.getCurrentPosition(currentLocation, error);
+const error = (err) => console.warn(`ERROR(${err.code}): ${err.message}`);
 
-        const latitude = -26.30;
-        const longitude = -48.85;
-        const divHoje = document.getElementById("hoje");
+const latitude = -26.30;
+const longitude = -48.85;
 
-        fetch("http://api.weatherapi.com/v1/forecast.json?key=870ea7f706f34eaf986213002221908&q=-26.30,-48.85&days=5&aqi=no&alerts=no")
-        .then(response => response.json()) 
-        .then(data => {
-            
-            
-            // console.log("data..current temperatura atual > ",data.current.temp_c);
-            // console.log("data.location.name> ",data.location.name);
-            // console.log("data.location.region> ",data.location.region);
-            // console.log("data.location.country> ",data.location.country);
-            // console.log("data.location.localtime> ",data.location.localtime);
-            
-            
-            const forecastObj = data.forecast.forecastday
+const divSemana = document.getElementById("semana");
+const divHoje = document.getElementById("hoje")
 
-            let html = "";
-            forecastObj.forEach(element => {
-                html +=`<div class="card" style="width: 18rem;">
-                <img src="${element.day.condition.icon}"
+fetch(`http://api.weatherapi.com/v1/forecast.json?key=870ea7f706f34eaf986213002221908&q=${latitude},${longitude}&days=5&aqi=no&alerts=no`)
+    .then(response => response.json())
+    .then(data => {
+
+        divHoje.innerHTML = `
+        <div class="card"">
+                <img src="${data.current.condition.icon}"
                 class="card-img-top" alt="...">
                 <div class="card-body">
-                    <p class="card-text">Day: ${element.date}</p>
-                    <p class="card-text">Max: ${element.day.maxtemp_c}<span>&#8451;</span> Min: ${element.day.mintemp_c}<span>&#8451;</span></p>
+                    <p>Temperature: ${data.location.name}, ${data.location.region},${data.location.country}</p>
+                    <p class="card-text">Current: ${data.current.temp_c}<span>&#8451;</span></p>
 
-                    <p class="card-text">Comment:${element.day.condition.text}</p>
+                    <p class="card-text">Comment: ${data.current.condition.text}</p>
                 </div>
               </div>`
 
 
-            });
-            
-            
-
-            divHoje.innerHTML = html
-            
-        })
+        const weekList = data.forecast.forecastday
         
-        const sleep = async timeInMs => setTimeout(() => {
-            console.log(`I'm here ${timeInMs/1000} Secs later...`)
-        }, timeInMs);
+        let htmlSemana = "";
+        weekList.forEach(element => {
+            htmlSemana += `<div class="card">
+                <img src="${element.day.condition.icon}"
+                class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Day: ${element.date}</p>
+                    <p class="card-text">Min: ${element.day.mintemp_c}<span>&#8451;</span> Max: ${element.day.maxtemp_c}<span>&#8451;</span> </p>
 
-        async function funcao01(dados){
-            let dadoProcessado = ""
-                await dados.forEach( element => {
-                      dadoProcessado +=`<div class="card" style="width: 18rem;">
-                            <img src="${element.day.condition.icon}"
-                            class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text">Day: ${element.date}</p>
-                                <p class="card-text">Min: ${element.day.mintemp_c}</p>
-                                <p class="card-text">Max: ${element.day.maxtemp_c}</p>
-                                <p class="card-text">Max: ${element.day.condition.text}</p>
-                            </div>
-                          </div>`
+                    <p class="card-text">Comment:${element.day.condition.text}</p>
+                </div>
+              </div>`
+        });
 
+        divSemana.innerHTML = htmlSemana
 
-                    });
-                    return dadoProcessado
-                    
-                }
+    })
--- assets/js/viaCepAPI.js
+++ assets/js/viaCepAPI.js
@@ -1,11 +1,8 @@
-const data = document.querySelector(".cep-api")
+const divCep = document.getElementById("cep")
 // const cep = prompt("Digite seu cep: ", 89209500)
-
-/* geolocation is available */
-var geolocation = Components.classes["@mozilla.org/geolocation;1"]
 
 fetch(`https://viacep.com.br/ws/${89209500}/json/`)
     .then(response => response.json())
     .then(data =>
-        console.log(data.logradouro, data.bairro, data.localidade, data.uf)
+        divCep.innerHTML += `<p>Localidade atual: ${data.logradouro}, ${data.bairro}, ${data.localidade}, ${data.uf}</p>`
     )
